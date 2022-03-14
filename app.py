e2d93b48271b7b2ce0a6d2641b68588f030f2ace
superstarcoder <dhanishnatarajan@gmail.com>
2022-03-13 20:05:41 -0700

    disabled schedule command and added cooldowns

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ links = json.load(open("gifs.json"))
 
 bot = commands.Bot(command_prefix="dog ")
 
-# @commands.cooldown(1, 5, commands.BucketType.user)
 @bot.command(name="hi")
 async def SendMessage(ctx):
 	await ctx.send('Hello!')
@@ -26,43 +25,40 @@ async def Gif(ctx):
 	await ctx.send(random.choice(links[ctx.invoked_with]))
 
 
-async def schedule_daily_message(h, m, s, msg, channelid):
-	while True:
-		now = datetime.datetime.now()
-		# then = now+datetime.timedelta(days=1)
-		then = now.replace(hour=h, minute=m, second=s)
-		if then < now:
-			then += datetime.timedelta(days=1)
-		wait_time = (then-now).total_seconds()
-		await asyncio.sleep(wait_time)
+# async def schedule_daily_message(h, m, s, msg, channelid):
+# 	while True:
+# 		now = datetime.datetime.now()
+# 		# then = now+datetime.timedelta(days=1)
+# 		then = now.replace(hour=h, minute=m, second=s)
+# 		if then < now:
+# 			then += datetime.timedelta(days=1)
+# 		wait_time = (then-now).total_seconds()
+# 		await asyncio.sleep(wait_time)
 
-		channel = bot.get_channel(channelid)
+# 		channel = bot.get_channel(channelid)
 
-		await channel.send(msg)
-		await channel.send(random.choice(links["play"]))
-		await asyncio.sleep(1)
+# 		await channel.send(msg)
+# 		await channel.send(random.choice(links["play"]))
+# 		await asyncio.sleep(1)
 
-# dog daily "good morning" 8 30
+# @bot.command(name="daily")
+# async def daily(ctx, mystr:str, hour:int, minute:int, second:int):
+# 	print(mystr, hour, minute, second)
 
-@bot.command(name="daily")
-async def daily(ctx, mystr:str, hour:int, minute:int, second:int):
-	print(mystr, hour, minute, second)
+# 	if not (0 < hour < 24 and 0 <= minute <= 60 and 0 <= second < 60):
+# 		raise commands.BadArgument()
 
-	if not (0 < hour < 24 and 0 <= minute <= 60 and 0 <= second < 60):
-		raise commands.BadArgument()
+# 	time = datetime.time(hour, minute, second)
+# 	timestr = time.strftime("%I:%M:%S %p")
+# 	await ctx.send(f"A daily message will be sent at {timestr} everyday in this channel.\nDaily message:\"{mystr}\"\nConfirm by simply saying: `yes`")
 
-	time = datetime.time(hour, minute, second)
-	timestr = time.strftime("%I:%M:%S %p")
-	await ctx.send(f"A daily message will be sent at {timestr} everyday in this channel.\nDaily message:\"{mystr}\"\nConfirm by simply saying: `yes`")
+# @daily.error
+# async def daily_error(ctx, error):
+# 	if isinstance(error, commands.BadArgument):
+# 		await ctx.send("""Incorrect format. Use the command this way: `dog daily "message" hour minute second`.
+# For example: `dog daily "good morning" 22 30 0` for a message to be sent at 10:30 everyday""")
 
-@daily.error
-async def daily_error(ctx, error):
-	if isinstance(error, commands.BadArgument):
-		await ctx.send("""Incorrect format. Use the command this way: `dog daily "message" hour minute second`.
-For example: `dog daily "good morning" 22 30 0` for a message to be sent at 10:30 everyday""")
-
-# dog speak hello world!!
-
+@commands.cooldown(1, 3, commands.BucketType.user)
 @bot.command(name='speak')
 async def speak(ctx, *args):
 	msg = " ".join(args)
@@ -103,6 +99,19 @@ async def support(ctx):
 	
 	await ctx.send("hi", view=myview)
 
+@bot.command()
+async def test(ctx):
+	hi = Button(label="click me", style=ButtonStyle.green)
+
+	async def hi_callback(interaction):
+		await interaction.response.send_message("Hello!")
+
+	hi.callback = hi_callback
+	myview = View(timeout=180)
+	myview.add_item(hi)
+	await ctx.send("hi", view=myview)
+
+
 @bot.event
 async def on_command_error(ctx, error):
 	if isinstance(error, commands.CommandOnCooldown):
@@ -114,5 +123,7 @@ async def on_ready():
 	print(f"Loggined in as: {bot.user.name}")
 	# await schedule_daily_message()
 
+TOKEN = "OTEwNzAyNzQyMDM1NjQwMzgw.YZWsBQ.HAUSHkv0gcmW_7IWEdkbhiAQsDo"
+# TOKEN = os.environ["DISCORD_TOKEN"]
 if __name__ == '__main__':
-	bot.run(os.environ["DISCORD_TOKEN"])
+	bot.run(TOKEN)
